d497b2f02a0cae5ef35b9e23dfac94e92dc85d3a
IrinaBelenko <118209690+IrinaBelenko@users.noreply.github.com>
2024-01-20 18:42:46 +0200

    custom browser

--- app/src/main/java/com/example/custombrowser/MainActivity.kt
+++ app/src/main/java/com/example/custombrowser/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.custombrowser
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val webView = findViewById<WebView>(R.id.webView)
+        webView.webViewClient = WebViewClient()
+        intent?.let {
+            val url = it.data
+            webView.loadUrl(url.toString())
+        }
+
     }
 }
